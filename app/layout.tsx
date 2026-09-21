dc749f334166f3f72eeb0cb61817ae6dc6d73c29
Maneth-k <manethkaveen0@gmail.com>
2026-09-21 09:35:28 +0530

    added a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,16 @@ const cormorant = Cormorant_Garamond({
 export const metadata: Metadata = {
   title: "Dinithi & Pulith - Wedding Invitation",
   description: "Celebrate the marriage of Dinithi & Pulith",
+  icons: {
+    icon: [
+      { url: "/pd-logo.png", type: "image/png" },
+      { url: "/icon.png", type: "image/png" },
+    ],
+    apple: [
+      { url: "/pd-logo.png", sizes: "180x180", type: "image/png" },
+    ],
+    shortcut: "/pd-logo.png",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
@@ -32,6 +42,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} ${cormorant.variable} h-full antialiased`}
     >
       <head>
+        <link rel="icon" href="/pd-logo.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="/pd-logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
